66400c06b0fce409f73629af25031715466e75f5
Lucas Capistrant <lucas.o.capistrant@gmail.com>
2026-03-11 09:49:06 -0500

    Default vue boilerplate cleanup + basic styling/fonts/typeography

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,18 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import Counter from './components/Counter.vue'
+import HeaderComponent from './components/Header.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <Counter></Counter>
-    </div>
+    <HeaderComponent/>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <body>
+    <h1>Heading H1</h1>
+    <h2>Heading H2</h2>
+    <h3>Heading H3</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam doloribus neque voluptate rem officiis labore aspernatur accusantium consequatur voluptates excepturi!</p>
+  </body>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<script setup>
+//import "ComponentName" from './componentname.vue';
+</script>
+
+<template>
+<header>Hello World</header>
+</template>
+
+<style scoped>
+header {
+    background-color: red;
+}
+</style>
+
