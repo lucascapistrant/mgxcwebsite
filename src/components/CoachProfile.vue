f9f6718182f0a6b1d97608e63cd0b4d67e800452
Lucas Capistrant <lucas.o.capistrant@gmail.com>
2026-07-13 16:56:54 -0500

    coaches page

---
+++ src/components/CoachProfile.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  name: String,
+  type: String,
+  photo: String,
+  contactInfo: String,
+  bio: String
+})
+</script>
+
+<template>
+<div class="profile">
+    <div class="profile-photo">
+        <img :src="photo" :alt="name" />
+    </div>
+    <div class="profile-info">
+        <h3 class="coach-name">{{ name }}</h3>
+        <p v-if="contactInfo" class="coach-contact-info">{{ contactInfo }}</p>
+        <p v-if="bio" class="coach-bio">{{ bio }}</p>
+    </div>
+</div>
+</template>
+
+<style scoped>
+.profile {
+    width: 100%;
+    max-width: 400px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+}
+
+.profile-photo {
+    width: 100%;
+    img {
+        width: 90%;
+        max-width: 300px;
+        aspect-ratio: 1/1;
+        object-fit: cover;
+        border-radius: 50%;
+    }
+}
+
+.coach-name {
+    font-size: 2rem;
+    font-weight: normal;
+    margin: 1rem 0 0.5rem 0;
+}
+
+.coach-type, .coach-contact-info, .coach-bio {
+    margin: 1.5rem 0;
+}
+
+.coach-contact-info {
+    color: var(--color-secondary);
+}
+
+.coach-bio {
+    font-style: italic;
+}
+</style>
--- src/views/CoachesView.vue
+++ src/views/CoachesView.vue
@@ -1,11 +1,100 @@
 <script setup>
+import CoachProfile from '../components/CoachProfile.vue'
+import { ref, onMounted, computed } from 'vue'
+import fm from 'front-matter';
 
+const coaches = ref([])
+
+const headCoaches = computed(() => {
+    return coaches.value.filter(coach => coach.type == 'Head Coach');
+});
+
+const assistantCoaches = computed(() => {
+    return coaches.value.filter(coach => coach.type == 'Assistant Coach');
+});
+
+onMounted(async() => {
+    const fileModules = import.meta.glob('../content/coaches/*.md', {
+        query: '?raw',
+        import: 'default',
+        eager: true
+    })
+
+    const loadedCoaches = [];
+
+    for (const path in fileModules) {
+        const rawText = fileModules[path];
+        const parsed = fm(rawText);
+        
+        if (parsed.attributes.coach_name) {
+            loadedCoaches.push({
+                name: parsed.attributes.coach_name,
+                type: parsed.attributes.coach_type || 'Assistant Coach',
+                photo: parsed.attributes.coach_photo || '',
+                contactInfo: parsed.attributes.coach_contact_info || '',
+                bio: parsed.attributes.coach_bio || ''
+            });
+        }
+    }
+    coaches.value = loadedCoaches;
+});
 </script>
 
 <template>
-    <h2>Coaches, Hello World!</h2>
+    <section>
+        <h2 class="page-header">Head Coaches</h2>
+        <div class="coaches">
+            <CoachProfile
+                v-for="(coach, index) in headCoaches"
+                :key="index"
+                :name="coach.name"
+                :type="coach.type"
+                :photo="coach.photo"
+                :contactInfo="coach.contactInfo"
+                :bio="coach.bio"
+                class="coach-profile"
+            />
+        </div>
+    </section>
+    <section>
+        <h2 class="page-header">Assistant Coaches</h2>
+        <div class="coaches">
+            <CoachProfile
+                v-for="(coach, index) in assistantCoaches"
+                :key="index"
+                :name="coach.name"
+                :type="coach.type"
+                :photo="coach.photo"
+                :contactInfo="coach.contactInfo"
+                :bio="coach.bio"
+                class="coach-profile"
+            />
+        </div>
+    </section>
 </template>
 
 <style scoped>
+.coaches {
+    width: 100%;
+    max-width: 1000px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 30px;
+    justify-items: center;
+}
 
+.coaches:last-child {
+    margin-bottom: 30px;
+}
+
+@media screen and (min-width: 768px) {
+    .coaches {
+        grid-template-columns: 1fr 1fr;
+    }
+
+    .coach-profile:nth-child(odd):last-child {
+        grid-column: span 2;
+    }
+}
 </style>
