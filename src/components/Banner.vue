05b57b0a41224b3c83a5e6a86b547f4f063a1a83
Lucas Capistrant <lucas.o.capistrant@gmail.com>
2026-08-08 22:30:11 -0500

    Boosters page setup

---
+++ src/components/Banner.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+    title: String,
+    message: String
+})
+</script>
+
+<template>
+    <div class="banner">
+        <h2>{{ title }}</h2>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<style scoped>
+.banner {
+    width: 90%;
+    max-width: 500px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: var(--color-object);
+    border: 1px solid var(--color-secondary);
+    border-radius: 30px;
+    text-align: center;
+}
+
+h2, p {
+    color: var(--color-secondary);
+}
+
+p {
+    line-height: 1.5;
+}
+
+@media screen and (min-width: 768px) {
+    h2 {
+        font-size: 2.5rem;
+    }
+
+    p {
+        font-size: 2rem;
+    }
+}
+</style>
--- src/views/BoostersView.vue
+++ src/views/BoostersView.vue
@@ -1,9 +1,12 @@
 <script setup>
-
+import Banner from '@/components/Banner.vue'
 </script>
 
 <template>
-    <h2>Boosters, Hello World!</h2>
+    <h2 class="page-header">Boosters Page</h2>
+    <Banner title="Website Under Construction"
+            message="We are currently working on updating this page. Please check back soon for the latest information and updates."
+    />
 </template>
 
 <style scoped>
